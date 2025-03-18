7a52267cdfdcedb3a8f1187908e8ef6f6ff8fd66
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2025-03-18 18:35:10 +0100

    lab4(configured jdbc:mysql/jpa)

--- IS_Labs/IS_Lab4_JPA/src/main/java/com/lg/Main.java
+++ IS_Labs/IS_Lab4_JPA/src/main/java/com/lg/Main.java
@@ -1,7 +1,12 @@
 package com.lg;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
 public class Main {
     public static void main(String[] args){
         System.out.println("JPA project");
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("Hibernate_JPA");
     }
 }
