89d1eb8d8690b3bcd338f1c5541f9df22bdb6815
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2025-03-18 18:42:33 +0100

    lab4.2.1-3

--- IS_Labs/IS_Lab4_JPA/src/main/java/com/lg/Main.java
+++ IS_Labs/IS_Lab4_JPA/src/main/java/com/lg/Main.java
@@ -1,5 +1,6 @@
 package com.lg;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -8,5 +9,14 @@ public class Main {
     public static void main(String[] args){
         System.out.println("JPA project");
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("Hibernate_JPA");
+        EntityManager em = factory.createEntityManager();
+        em.getTransaction().begin();
+
+        User u1 = new User("test_1","test_1","Andrzej","Kowalski");
+        em.persist(u1);
+        em.getTransaction().commit();
+        em.close();
+
+        factory.close();
     }
 }
---
+++ IS_Labs/IS_Lab4_JPA/src/main/java/com/lg/User.java
@@ -0,0 +1,65 @@
+package com.lg;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+
+    public User() {}
+    public User(String login, String password, String firstName, String lastName) {
+        this.login = login;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
