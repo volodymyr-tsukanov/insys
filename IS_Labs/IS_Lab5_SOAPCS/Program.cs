cf4431cf85f1156ac72baa880149664dc53d8450
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2025-03-25 19:15:42 +0100

    lab5.2.1

---
+++ IS_Labs/IS_Lab5_SOAPCS/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS_Lab5_SOAPCS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1");
+            Console.ReadLine();
+        }
+    }
+}
