3a61fc89fa19a49f63981ba8e4b0b78401c08d48
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2025-03-25 19:03:01 +0100

    lab5.1

---
+++ IS_Labs/IS_Lab5_SOAP/MySOAP/src/main/java/com/soapsoap/MyFirstWS.java
@@ -0,0 +1,34 @@
+package com.soapsoap;
+
+import javax.jws.WebService;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+@WebService(endpointInterface = "com.soapsoap.MyFirstSOAPInterface")
+public class MyFirstWS implements MyFirstSOAPInterface {
+    public String getHelloWorldAsString(String nazwa) {
+        String message = "Witaj " + nazwa + "!";
+        return message;
+    }
+
+    public long getDaysBetweenDates(String date1, String
+            date2) {
+        long numberOfDays = 0;
+        DateTimeFormatter dtf =
+                DateTimeFormatter.ofPattern("dd MM yyyy HH:mm:ss");
+        try {
+            LocalDateTime tempdate1 = LocalDate.parse(date1,
+                    dtf).atStartOfDay();
+            LocalDateTime tempdate2 = LocalDate.parse(date2,
+                    dtf).atStartOfDay();
+            numberOfDays = Duration.between(tempdate1,
+                    tempdate2).toDays();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return numberOfDays;
+    }
+}
